35f8dc388b44b26e05653c9888d0fc6d24ef2625
Eric Turriff <ericturf@Erics-MacBook-Pro.local>
2018-03-18 12:07:03 -0400

    initial implementation of interview questions

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,10 +1,27 @@
-import helloWorld from '../src/index';
+import QuestionGenerator from "../src/index";
 
-describe(`my first test`, () => {
+describe(`QuestionGenerator`, () => {
+  describe(`.interview`, () => {
+    it(`should be a function of arity 1`, () => {
+      expect(QuestionGenerator.interview).toBeInstanceOf(Function);
+      expect(QuestionGenerator.interview).toHaveLength(1);
+    });
+    it(`should return an empty string if no keyword is given`, () => {
+      //Act
+      const result = QuestionGenerator.interview();
 
-  it(`must say hello world`, () => {
-    const actual = helloWorld();
-    expect(actual).toBe(`hello world`);
+      //Assert
+      expect(result).toEqual(``);
+    });
+    it(`should return a random string containing the supplied keyword`, () => {
+      //Arrange
+      const keyword = `turtles`;
+
+      //Act
+      const result = QuestionGenerator.interview(keyword);
+      console.log(result);
+      //Assert
+      expect(result.includes(keyword)).toBeTruthy();
+    });
   });
-
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-export default () => {
-  return `hello worlds`;
+const interview = keyword => {
+  if (!keyword) return ``;
+  const questions = [
+    `Would you be interested working with ${keyword}?`,
+    `Have you ever shown interest in ${keyword}?`,
+    `Is working with ${keyword} something that interests you?`,
+    `Have you ever considered working with ${keyword}`
+  ];
+
+  const getRandomIndex = () => Math.floor(Math.random() * questions.length);
+
+  return questions[getRandomIndex()];
+};
+
+export default {
+  interview
 };
